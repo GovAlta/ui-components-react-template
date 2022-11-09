f8a9200800ddc3ccc97cd3d14cd5cf0094de600a
Chris Olsen <olsen.chris@gmail.com>
2022-11-09 09:19:37 -0700

    update to release version

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import {
   GoAMicrositeHeader,
   GoAAppFooter,
   GoAPageBlock,
-  GoAOneColumnLayout,
   GoAAppFooterMetaSection,
-} from '@abgov/react-components';
-import { Outlet } from 'react-router-dom';
+  GoAOneColumnLayout,
+} from "@abgov/react-components";
+import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
     <GoAOneColumnLayout>
       <section slot="header">
-        <GoAMicrositeHeader type="beta" version="React 1.0" feedbackUrl="https://github.com/GovAlta/ui-components/issues/new/choose" />
-        <GoAAppHeader url="/" heading="Design system templates" maxContentWidth="100%" />
+        <GoAMicrositeHeader
+          type="beta"
+          version="React 1.0"
+          feedbackUrl="https://github.com/GovAlta/ui-components/issues/new/choose"
+        />
+        <GoAAppHeader
+          url="/"
+          heading="Design system templates"
+          maxContentWidth="100%"
+        />
       </section>
 
       <GoAPageBlock width="904px">
@@ -23,9 +31,15 @@ export function App() {
       <section slot="footer">
         <GoAAppFooter maxContentWidth="100%">
           <GoAAppFooterMetaSection>
-            <a href="https://goa-dio.slack.com/archives/C02PLLT9HQ9">Get help</a>
-            <a href="https://goa-dio.atlassian.net/wiki/spaces/DS/pages/2342813697/Design+System+Drop-in+hours">Drop-in Hours</a>
-            <a href="https://github.com/GovAlta/ui-components/issues/new/choose">Contribute</a>
+            <a href="https://goa-dio.slack.com/archives/C02PLLT9HQ9">
+              Get help
+            </a>
+            <a href="https://goa-dio.atlassian.net/wiki/spaces/DS/pages/2342813697/Design+System+Drop-in+hours">
+              Drop-in Hours
+            </a>
+            <a href="https://github.com/GovAlta/ui-components/issues/new/choose">
+              Contribute
+            </a>
           </GoAAppFooterMetaSection>
         </GoAAppFooter>
       </section>
